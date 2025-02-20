1ce5bb3ecb6a024fc0a560a4df9dbc6ab22627df
Randy Li <ayaka@soulik.info>
2025-02-21 00:30:00 +0800

    netio: tcp: initial userspace tcp stack
    The early version of userspace TCP stack

---
+++ netio/inc/userspace_tcp_stack.hpp
@@ -0,0 +1,626 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-only
+ * SPDX-FileCopyrightText: Hsia-Jun(Randy) Li
+ */
+
+#ifndef USERSPACE_TCP_STACK_HPP_
+#define USERSPACE_TCP_STACK_HPP_
+
+#include <algorithm>
+#include <arpa/inet.h>
+#include <chrono>
+#include <cstdint>
+#include <cstring>
+#include <deque>
+#include <forward_list>
+
+#if PARALLEL
+#include <execution>
+#define PAR std::execution::par,
+#else
+#define PAR
+#endif
+
+#include <asio.hpp>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <optional>
+
+#include "net_packet.hpp"
+
+namespace celaratcp {
+namespace netio {
+
+enum class TcpConnectionState
+{
+  CLOSED,
+  LISTEN,
+  SYN_SENT,
+  SYN_RECEIVED,
+  ESTABLISHED,
+  FIN_WAIT_1,
+  FIN_WAIT_2,
+  CLOSE_WAIT,
+  CLOSING,
+  LAST_ACK,
+  TIME_WAIT
+};
+
+enum class TcpStackState
+{
+  SUCCESS,
+  DROP,
+  ERR_ARGUMENT,
+  ERR_WRONG_FAMILY,
+  ERR_WRONG_PROTOCOL,
+  ERR_NO_SUCH_SERVICE,
+};
+
+enum class TcpPacketType
+{
+  SYN,
+  SYN_ACK,
+  ACK,
+  FIN,
+  FIN_ACK,
+  RST
+};
+
+template <typename AddrType> class TcpConnection
+{
+protected:
+  /* network bytes order, cache data */
+  const typename AddrType::bytes_type local_addr_ND_;
+  const uint_fast16_t local_port_ND_;
+
+public:
+  TcpConnectionState state_;
+  const AddrType remote_addr_;
+  const uint_fast16_t remote_port_;
+
+protected:
+  typename AddrType::bytes_type remote_addr_ND_;
+  uint_fast16_t remote_port_ND_;
+
+  // The outgoing sequence number
+  uint32_t sequenceN;
+  // The outgoing ACK number
+  uint32_t ackN;
+
+  std::chrono::time_point<std::chrono::steady_clock> last_activity_;
+
+public:
+  using TcpConnectionCtorArgs = std::tuple<const AddrType &, uint_fast16_t,
+                                           const AddrType &, uint_fast16_t>;
+
+  TcpConnection(const AddrType &local_addr, uint_fast16_t local_port,
+                const AddrType &remote_addr, uint_fast16_t remote_port)
+      : local_addr_ND_(local_addr.to_bytes()),
+        local_port_ND_(htons(local_port)), remote_addr_(remote_addr),
+        remote_port_(remote_port), remote_addr_ND_(remote_addr.to_bytes()),
+        remote_port_ND_(htons(remote_port)), sequenceN(0), ackN(0)
+  {
+  }
+
+  TcpConnection(AddrType remoteAddr, uint_fast16_t remotePort)
+      : remote_addr_(remoteAddr), remote_port_(remotePort), sequenceN(0),
+        ackN(0)
+  {
+    // will remove
+  }
+  ~TcpConnection() = default;
+
+  virtual void
+  UpdateRecvSeq(TcpPacketType type, uint32_t seq,
+                uint32_t payload_size = 0) noexcept
+  {
+    switch (type) {
+    case TcpPacketType::SYN:
+    case TcpPacketType::SYN_ACK:
+      if (payload_size == 0) {
+        payload_size = 1;
+      }
+      ackN = seq + payload_size;
+
+      break;
+    case TcpPacketType::ACK:
+      break;
+    default:
+      break;
+    }
+  }
+
+  virtual void
+  UpdateRecvAck(TcpPacketType type, uint32_t ack) noexcept
+  {
+    // do nothing
+  }
+
+  virtual void
+  UpdateSentSeq(TcpPacketType type, uint32_t seq = 0,
+                uint32_t payload_size = 0) noexcept
+  {
+    switch (type) {
+    case TcpPacketType::SYN:
+    case TcpPacketType::SYN_ACK:
+      if (payload_size == 0) {
+        payload_size = 1;
+      }
+      if (seq == 0) {
+        sequenceN += payload_size;
+      } else {
+        sequenceN = seq + payload_size;
+      }
+
+      break;
+    case TcpPacketType::ACK:
+      break;
+    default:
+      break;
+    }
+  }
+
+  virtual void
+  SetPacketMetaData(std::shared_ptr<NetPacket> packet, uint32_t seq,
+                    uint32_t ack)
+  {
+    packet->meta.data[0] = seq;
+    packet->meta.data[1] = ack;
+  }
+
+  virtual void Established() {};
+
+  TcpConnection(TcpConnection &&) = default;
+  TcpConnection &operator=(TcpConnection &&) = default;
+
+protected:
+  // IPv4 specialization
+  template <
+      typename T = AddrType,
+      typename std::enable_if_t<std::is_same_v<T, asio::ip::address_v4>, int>
+      = 0>
+  bool
+  ConstructIpHdr(NetPacket &hdr, const std::size_t payload_size = 0,
+                 uint8_t ttl = 64) noexcept
+  {
+    // Construct IPv4 header
+    auto data = hdr.GetData();
+    if (data.size() < kIpv4HdrSize) {
+      return false;
+    }
+
+    data[0] = 0x45; // Version and IHL
+    data[1] = 0x00; // DSCP and ECN
+    *reinterpret_cast<uint16_t *>(data.data() + 2) = htons(
+        kIpv4HdrSize + kTcpHdrMinimalSize + payload_size); // Total Length
+    *reinterpret_cast<uint16_t *>(data.data() + 4) = 0;    // Identification
+    data[6] = 0x40;        // Flags and Fragment Offset
+    data[8] = ttl;         // TTL
+    data[9] = IPPROTO_TCP; // Protocol
+    // Source and destination addresses
+    std::copy(local_addr_ND_.begin(), local_addr_ND_.end(), data.data() + 12);
+    std::copy(remote_addr_ND_.begin(), remote_addr_ND_.end(),
+              data.data() + 16);
+    // clear checksum
+    *reinterpret_cast<uint16_t *>(data.data() + 10) = 0;
+
+    hdr.SetUsedBytes(kIpv4HdrSize);
+    return true;
+  }
+
+  // IPv6 specialization
+  template <
+      typename T = AddrType,
+      typename std::enable_if_t<std::is_same_v<T, asio::ip::address_v6>, int>
+      = 0>
+  bool
+  ConstructIpHdr(NetPacket &hdr, const std::size_t payload_size = 0,
+                 uint8_t ttl = 64) noexcept
+  {
+    // Construct IPv6 header
+    auto data = hdr.GetData();
+    if (data.size() < kIpv6HdrSize) {
+      return false;
+    }
+
+    data[0] = 0x60; // Version and Traffic Class
+    data[1] = 0;    // Traffic Class and Flow Label
+    data[2] = 0;    // Flow Label
+    data[3] = 0;    // Flow Label
+    *reinterpret_cast<uint16_t *>(data.data() + 4) = htons(
+        kIpv6HdrSize + kTcpHdrMinimalSize + payload_size); // Payload Length
+    data[6] = IPPROTO_TCP;                                 // Next Header
+    data[7] = ttl;                                         // Hop Limit
+    // Source and destination addresses
+    std::copy(local_addr_ND_.begin(), local_addr_ND_.end(), data.data() + 8);
+    std::copy(remote_addr_ND_.begin(), remote_addr_ND_.end(),
+              data.data() + 24);
+
+    hdr.SetUsedBytes(kIpv6HdrSize);
+    return true;
+  }
+
+public:
+  void
+  FreshActivity()
+  {
+    last_activity_ = std::chrono::steady_clock::now();
+  }
+
+  virtual void
+  FillPacketIpTcpHdr(TcpPacketType packetType, NetPacket &hdr,
+                     std::shared_ptr<NetPacket> payload = nullptr)
+  {
+    switch (packetType) {
+    case TcpPacketType::ACK:
+      if (payload) {
+        ConstructIpHdr(hdr, payload->GetUsedBytes());
+      } else {
+        ConstructIpHdr(hdr);
+      }
+      break;
+    default:
+      if (payload) {
+        // TODO: drop payload
+      }
+      ConstructIpHdr(hdr);
+      break;
+    }
+
+    auto data = hdr.GetData().data() + hdr.GetUsedBytes();
+    // Construct TCP header
+    *reinterpret_cast<uint16_t *>(data) = local_port_ND_;
+    *reinterpret_cast<uint16_t *>(data + 2) = remote_port_ND_;
+
+    uint32_t seq, ack;
+    seq = hdr.meta.data[0];
+    ack = hdr.meta.data[1];
+    *reinterpret_cast<uint32_t *>(data + 4)
+        = htonl(sequenceN + seq); // Sequence number
+    *reinterpret_cast<uint32_t *>(data + 8)
+        = htonl(ackN + ack); // Acknowledgment number
+    data[12] = 0x50;         // Data offset
+
+    *reinterpret_cast<uint16_t *>(data + 14) = htons(4000); // Window size
+    *reinterpret_cast<uint16_t *>(data + 16) = 0;           // Checksum
+    *reinterpret_cast<uint16_t *>(data + 18) = 0;           // Urgent pointer
+
+    switch (packetType) {
+    case TcpPacketType::SYN:
+      data[13] = 0x02; // Flag
+      break;
+    case TcpPacketType::SYN_ACK:
+      data[13] = 0x12; // Flags
+      break;
+    case TcpPacketType::ACK:
+      data[13] = 0x10; // Flags
+      break;
+    case TcpPacketType::FIN:
+      data[13] = 0x01; // Flags
+      break;
+    case TcpPacketType::FIN_ACK:
+      data[13] = 0x11; // Flags
+      break;
+    case TcpPacketType::RST:
+      data[12] = 0;    // RST should not have a payload
+      data[13] = 0x04; // Flags (RST)
+      break;
+    default:
+      throw std::invalid_argument("Invalid TcpPacketType");
+    }
+
+    hdr.SetUsedBytes(hdr.GetUsedBytes() + kTcpHdrMinimalSize);
+  }
+
+  template <typename, typename> friend class TcpService;
+  template <typename, typename, typename> friend class UserspaceTcpStack;
+};
+
+template <typename AddrType,
+          typename TcpConnectionT = TcpConnection<AddrType> >
+class TcpService
+{
+protected:
+  AddrType local_addr_;
+  uint_fast16_t port_;
+  std::forward_list<TcpConnectionT> connections_list_;
+
+public:
+  using TcpServiceCtorArgs = std::tuple<AddrType &, uint_fast16_t>;
+
+  TcpService(AddrType &addr, uint_fast16_t port)
+      : local_addr_(addr), port_(port)
+  {
+  }
+  ~TcpService() = default;
+
+  uint_fast16_t
+  GetPort() const
+  {
+    return port_;
+  }
+
+  const AddrType &
+  GetLocalAddr() const
+  {
+    return local_addr_;
+  }
+
+  virtual bool
+  AddConnection(TcpConnectionT &&conn)
+  {
+    auto it
+        = std::find_if(connections_list_.cbegin(), connections_list_.cend(),
+                       [&](const TcpConnectionT &c) {
+                         return c.remote_addr_ == conn.remote_addr_
+                                && c.remote_port_ == conn.remote_port_;
+                       });
+    if (it == connections_list_.end()) {
+      connections_list_.push_front(std::move(conn));
+      return true;
+    }
+    return false;
+  }
+
+  virtual bool
+  RemoveConnection(AddrType remoteAddr, uint_fast16_t remotePort)
+  {
+    auto it = std::find_if(connections_list_.begin(), connections_list_.end(),
+                           [&](const TcpConnectionT &conn) {
+                             return conn.remote_addr_ == remoteAddr
+                                    && conn.remote_port_ == remotePort;
+                           });
+    if (it != connections_list_.end()) {
+      connections_list_.erase_after(it);
+      return true;
+    }
+    return false;
+  }
+
+  virtual bool
+  RemoveConnection(AddrType remoteAddr)
+  {
+    bool found = false;
+    for (auto it = connections_list_.begin(); it != connections_list_.end();) {
+      if (it->remote_addr_ == remoteAddr) {
+        it = connections_list_.erase_after(it);
+        found = true;
+      } else {
+        ++it;
+      }
+    }
+    return found;
+  }
+
+  virtual std::optional<std::reference_wrapper<TcpConnectionT> >
+  GetConnection(AddrType remoteAddr, uint_fast16_t remotePort)
+  {
+    auto it = std::find_if(connections_list_.begin(), connections_list_.end(),
+                           [&](const TcpConnectionT &conn) {
+                             return conn.remote_addr_ == remoteAddr
+                                    && conn.remote_port_ == remotePort;
+                           });
+    if (it != connections_list_.end()) {
+      return std::optional<std::reference_wrapper<TcpConnectionT> >(
+          std::ref(*it));
+    }
+    return std::nullopt;
+  }
+};
+
+template <typename AddrType, typename TcpConnectionT = TcpConnection<AddrType>,
+          typename TcpServiceT = TcpService<AddrType, TcpConnectionT> >
+class UserspaceTcpStack
+{
+  static_assert(std::is_base_of_v<TcpConnection<AddrType>, TcpConnectionT>,
+                "TcpConnectionT must derive from TcpConnection<AddrType>");
+  static_assert(std::is_base_of_v<TcpService<AddrType>, TcpServiceT>
+                    || std::is_base_of_v<TcpService<AddrType, TcpConnectionT>,
+                                         TcpServiceT>,
+                "TcpServiceT must derive from TcpService");
+
+protected:
+  std::mutex mutex_;
+  // Remove connections_list_ from here
+  std::forward_list<std::shared_ptr<TcpServiceT> > services_;
+  AddrType local_addr_;
+
+private:
+  template <typename CharIterator> class Uint16Iterator
+  {
+  public:
+    using iterator_category = std::forward_iterator_tag;
+    using value_type = uint16_t;
+    using difference_type = std::ptrdiff_t;
+    using pointer = uint16_t *;
+    using reference = uint16_t &;
+
+    Uint16Iterator(CharIterator it) : it_(it) {}
+
+    uint16_t
+    operator*() const
+    {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+      return static_cast<uint16_t>(static_cast<uint8_t>(*(it_ + 1)) << 8)
+             | static_cast<uint8_t>(*it_);
+#else
+      return *reinterpret_cast<const uint16_t *>(&*it_);
+#endif
+    }
+    Uint16Iterator &
+    operator++()
+    {
+      it_ += 2;
+      return *this;
+    }
+
+    Uint16Iterator
+    operator++(int)
+    {
+      Uint16Iterator tmp = *this;
+      it_ += 2;
+      return tmp;
+    }
+
+    bool
+    operator==(const Uint16Iterator &other) const
+    {
+      return it_ == other.it_;
+    }
+
+    bool
+    operator!=(const Uint16Iterator &other) const
+    {
+      return it_ != other.it_;
+    }
+
+  private:
+    CharIterator it_;
+  };
+
+  template <typename Iterator>
+  uint_fast16_t
+  RangeCheckSum(Iterator begin, Iterator end)
+  {
+    uint_fast32_t sum
+        = std::reduce(Uint16Iterator(begin), Uint16Iterator(end), 0u);
+
+    while (sum >> 16)
+      sum = (sum & 0xFFFF) + (sum >> 16);
+
+    return static_cast<uint16_t>(~sum);
+  }
+
+protected:
+  template <
+      typename T = AddrType,
+      typename std::enable_if_t<std::is_same_v<T, asio::ip::address_v4>, int>
+      = 0>
+  std::pair<bool, std::size_t>
+  ParseIpHeader(std::shared_ptr<NetPacket> packet, std::size_t &packet_length,
+                T &src_addr, T &dst_addr)
+  {
+    auto data = packet->GetData().data();
+
+    packet_length = static_cast<std::size_t>(
+        ntohs(*reinterpret_cast<const uint16_t *>(data + 2)));
+
+    asio::ip::address_v4::bytes_type bytes
+        = { data[12], data[13], data[14], data[15] };
+    src_addr = asio::ip::address_v4(bytes);
+    bytes = { data[16], data[17], data[18], data[19] };
+    dst_addr = asio::ip::address_v4(bytes);
+
+    auto ipHeaderLength = ((static_cast<uint8_t>(data[0])) & 0x0F) * 4;
+
+    return { true, ipHeaderLength };
+  }
+
+  template <
+      typename T = AddrType,
+      typename std::enable_if_t<std::is_same_v<T, asio::ip::address_v6>, int>
+      = 0>
+  std::pair<bool, std::size_t>
+  ParseIpHeader(std::shared_ptr<NetPacket> packet, std::size_t &packet_length,
+                T &src_addr, T &dst_addr)
+  {
+    auto data = packet->GetData().data();
+
+    packet_length = static_cast<std::size_t>(
+        ntohs(*reinterpret_cast<const uint16_t *>(data + 4)));
+    asio::ip::address_v6::bytes_type bytes;
+    std::copy(data + 8, data + 24, bytes.begin());
+    src_addr = asio::ip::address_v6(bytes);
+    std::copy(data + 24, data + 40, bytes.begin());
+    dst_addr = asio::ip::address_v6(bytes);
+    constexpr auto ipHeaderLength = 40; // IPv6 header is always 40 bytes
+    return { true, ipHeaderLength };
+  }
+
+public:
+  UserspaceTcpStack() {}
+  ~UserspaceTcpStack() = default;
+
+  void
+  SetLocalAddress(const AddrType &addr)
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    local_addr_ = addr;
+  }
+
+  auto
+  AddSimpleService(uint_fast16_t port)
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    auto service = std::make_shared<TcpServiceT>(local_addr_, port);
+    services_.emplace_front(service);
+    return std::weak_ptr<TcpServiceT>(service);
+  }
+
+  bool
+  RemoveService(uint_fast16_t port)
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    auto it = std::find_if(services_.begin(), services_.end(),
+                           [port](std::shared_ptr<TcpServiceT> &it) {
+                             return it->GetPort() == port;
+                           });
+    if (it != services_.end()) {
+      services_.remove(it);
+      return true;
+    }
+    return false;
+  }
+
+  bool
+  RemoveService(std::weak_ptr<TcpServiceT> service_weak)
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    auto service = service_weak.lock();
+    if (!service) {
+      return false;
+    }
+    auto it = std::find(services_.begin(), services_.end(), service);
+    if (it != services_.end()) {
+      services_.remove(it);
+      return true;
+    }
+    return false;
+  }
+
+  bool
+  RemoveConnection(std::shared_ptr<TcpServiceT> service, AddrType addr)
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    auto it = std::find(services_.begin(), services_.end(), service);
+    if (it != services_.end()) {
+      return it->RemoveConnection(addr);
+    }
+    return false;
+  }
+
+  bool
+  RemoveConnection(std::shared_ptr<TcpServiceT> service, AddrType addr,
+                   uint_fast16_t port)
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    auto it = std::find(services_.begin(), services_.end(), service);
+    if (it != services_.end()) {
+      return it->RemoveConnection(addr, port);
+    }
+    return false;
+  }
+
+  virtual uint32_t
+  InitialConnSeq(const AddrType &localAddr, const uint_fast16_t localPort,
+                 const AddrType &remoteAddr, const uint_fast16_t remotePort)
+  {
+    return 0;
+  }
+};
+
+} // namespace netio
+} // namespace celaratcp
+
+#endif
---
+++ netio/inc/userspace_tcp_stack_helper.hpp
@@ -0,0 +1,312 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-only
+ * SPDX-FileCopyrightText: Hsia-Jun(Randy) Li
+ */
+
+#ifndef USERSPACE_TCP_STACK_HELPER_HPP_
+#define USERSPACE_TCP_STACK_HELPER_HPP_
+
+#include <asio/experimental/channel.hpp>
+#include <recycle/shared_pool.hpp>
+#include <utility>
+
+#include "userspace_tcp_stack.hpp"
+
+namespace celaratcp {
+namespace netio {
+
+template <typename AddrType, NetPacketContainer PacketContainer
+                             = std::vector<std::shared_ptr<NetPacket> > >
+class TcpConnectionChan : public TcpConnection<AddrType>
+{
+public:
+  // Use asio::experimental::channel_traits<asio::any_io_executor> for
+  // compatibility
+  using ChannelType = asio::experimental::channel<void(PacketContainer &&)>;
+
+  using typename TcpConnection<AddrType>::TcpConnectionCtorArgs;
+
+  TcpConnectionChan(const TcpConnectionCtorArgs &args,
+                    asio::any_io_executor &ex)
+      : TcpConnection<AddrType>(std::apply(
+            [](const auto &...params) {
+              return TcpConnection<AddrType>(params...);
+            },
+            args)),
+        rx_chan_(ex, 32)
+  {
+  }
+
+  TcpConnectionChan(TcpConnectionChan &&) = default;
+  TcpConnectionChan &operator=(TcpConnectionChan &&) = default;
+
+  void
+  submitRxChann(PacketContainer &&packets)
+  {
+    rx_chan_.async_send(std::move(packets), asio::use_awaitable);
+  }
+
+  asio::awaitable<PacketContainer>
+  fetchPackets()
+  {
+    PacketContainer packets;
+    co_await rx_chan_.async_receive(packets, asio::use_awaitable);
+    co_return packets;
+  }
+
+private:
+  ChannelType rx_chan_;
+};
+
+using shared_netbuf_pool_t
+    = std::shared_ptr<recycle::shared_pool<NetMemChunk> >;
+
+template <typename AddrType, typename TcpConnectionT, typename TcpServiceT,
+          typename NetworkIOObjectT>
+class AsyncUserspaceTcpStack
+    : public UserspaceTcpStack<AddrType, TcpConnectionT, TcpServiceT>
+{
+private:
+  static_assert(std::is_base_of_v<TcpConnectionChan<AddrType>, TcpConnectionT>,
+                "TcpConnectionT must derive from TcpConnectionChan<AddrType>");
+
+protected:
+  asio::any_io_executor &executor_;
+  NetworkIOObjectT &nout_;
+  shared_netbuf_pool_t hdr_pool_;
+
+  // Wrapper coroutine for sending packets downstream
+  template <typename ConstBufferSequence>
+  asio::awaitable<std::size_t>
+  tx_callback_(ConstBufferSequence &bufs)
+  {
+    co_return co_await asio::async_write(nout_, bufs, asio::use_awaitable);
+  }
+
+public:
+  // Accept executor by value (copy/move), not by reference_wrapper
+  AsyncUserspaceTcpStack(asio::any_io_executor &ex, NetworkIOObjectT &net_io,
+                         shared_netbuf_pool_t hdr_pool)
+      : UserspaceTcpStack<AddrType, TcpConnectionT, TcpServiceT>(),
+        executor_(ex), nout_(net_io), hdr_pool_(hdr_pool)
+  {
+  }
+  ~AsyncUserspaceTcpStack() = default;
+
+  template <typename NetPacket>
+  bool
+  FilterIncomingPacket(NetPacket &packet) const
+  {
+    if constexpr (std::is_same_v<AddrType, asio::ip::address_v4>) {
+      if (packet.GetUsedBytes() < (kIpv4HdrSize + kTcpHdrMinimalSize)) {
+        return false;
+      }
+      auto data = packet.GetData().data();
+      if (data[0] != 0x45) {
+        return false;
+      }
+      if (data[9] != IPPROTO_TCP) {
+        return false;
+      }
+    } else if constexpr (std::is_same_v<AddrType, asio::ip::address_v6>) {
+      if (packet.GetUsedBytes() < (kIpv6HdrSize + kTcpHdrMinimalSize)) {
+        return false;
+      }
+      auto data = packet.GetData().data();
+      if (data[0] != 0x60) {
+        return false;
+      }
+      if (data[6] != IPPROTO_TCP) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+    return true;
+  }
+
+  template <NetPacketContainer PacketContainer>
+  asio::awaitable<TcpStackState>
+  ProcessIncomingPackets(PacketContainer &packets)
+  {
+    auto total_bytes_used = std::accumulate(
+        packets.cbegin(), packets.cend(), std::size_t(0),
+        [](std::size_t sum, const std::shared_ptr<NetPacket> &packet) {
+          return sum + packet->GetUsedBytes();
+        });
+
+    auto packet = packets.front();
+    bool success;
+    std::size_t ip_payload_offset;
+    std::size_t packet_length;
+    AddrType src_addr, dst_addr;
+
+    std::tie(success, ip_payload_offset)
+        = this->ParseIpHeader(packet, packet_length, src_addr, dst_addr);
+    if (!success) {
+      co_return TcpStackState::ERR_WRONG_PROTOCOL;
+    }
+
+    std::size_t payloadOffset = ip_payload_offset + kTcpHdrMinimalSize;
+    if (total_bytes_used < payloadOffset) {
+      co_return TcpStackState::ERR_WRONG_PROTOCOL;
+    }
+    if (packet->GetUsedBytes() < payloadOffset) {
+      co_return TcpStackState::ERR_WRONG_PROTOCOL;
+    }
+    /* safely packet checking */
+    if (packet_length > total_bytes_used) {
+      co_return TcpStackState::ERR_WRONG_PROTOCOL;
+    }
+
+    auto data = packet->GetData().data() + ip_payload_offset;
+
+    auto dst_port = ntohs(*reinterpret_cast<const uint16_t *>(data + 2));
+    auto it = std::find_if(
+        this->services_.begin(), this->services_.end(),
+        [dst_port](const std::shared_ptr<TcpServiceT> &service) {
+          return service->GetPort() == dst_port;
+        });
+    if (it == this->services_.end()) {
+      co_return TcpStackState::ERR_NO_SUCH_SERVICE;
+    }
+    auto service = it->get();
+
+    auto src_port = ntohs(*reinterpret_cast<const uint16_t *>(data));
+    auto tcpFlags = static_cast<uint8_t>(data[13]);
+    if (tcpFlags == 0x12) {
+      // SYN-ACK
+      auto conn_op = service->GetConnection(src_addr, src_port);
+      if (conn_op) {
+        auto &conn = conn_op->get();
+        if (conn.state_ == TcpConnectionState::SYN_SENT) {
+          auto seqNum = ntohl(*reinterpret_cast<const uint32_t *>(data + 4));
+          conn.UpdateRecvSeq(TcpPacketType::SYN_ACK, seqNum);
+
+          auto reply = hdr_pool_->allocate();
+          conn.FillPacketIpTcpHdr(TcpPacketType::ACK, *reply);
+          std::list<asio::const_buffer> r = { reply->GetConstBuf() };
+
+          auto ret = co_await tx_callback_(r);
+          if (ret >= reply->GetUsedBytes()) {
+            conn.UpdateSentSeq(TcpPacketType::ACK, 0, 1);
+
+            conn.state_ = TcpConnectionState::ESTABLISHED;
+            conn.FreshActivity();
+            conn.Established();
+
+            co_return TcpStackState::SUCCESS;
+          }
+          co_return TcpStackState::DROP;
+        } else {
+          co_return TcpStackState::ERR_ARGUMENT;
+        }
+      }
+      co_return TcpStackState::ERR_ARGUMENT;
+    } else if (tcpFlags == 0x11) {
+      // FIN-ACK
+      auto conn_op = service->GetConnection(src_addr, src_port);
+      if (conn_op) {
+        auto &conn = conn_op->get();
+        if (conn.state_ == TcpConnectionState::ESTABLISHED) {
+          conn.state_ = TcpConnectionState::CLOSE_WAIT;
+          // TODO
+          co_return TcpStackState::SUCCESS;
+        } else {
+          co_return TcpStackState::ERR_ARGUMENT;
+        }
+      }
+      co_return TcpStackState::ERR_ARGUMENT;
+    } else if (tcpFlags == 0x10) {
+      // ACK
+      auto conn_op = service->GetConnection(src_addr, src_port);
+      if (conn_op) {
+        auto &conn = conn_op->get();
+        if (conn.state_ == TcpConnectionState::SYN_SENT) {
+          // wrong flag, should be SYN-ACK
+          co_return TcpStackState::ERR_ARGUMENT;
+        } else if (conn.state_ == TcpConnectionState::SYN_RECEIVED) {
+          conn.state_ = TcpConnectionState::ESTABLISHED;
+          conn.FreshActivity();
+          conn.Established();
+          // jump out state check
+        } else if (conn.state_ == TcpConnectionState::ESTABLISHED) {
+          // jump out state check
+        } else {
+          co_return TcpStackState::ERR_ARGUMENT;
+        }
+
+        auto ackNum = ntohl(*reinterpret_cast<const uint32_t *>(data + 8));
+        conn.UpdateRecvAck(TcpPacketType::ACK, ackNum);
+
+        auto seqNum = ntohl(*reinterpret_cast<const uint32_t *>(data + 4));
+
+        auto payloadSize = total_bytes_used - payloadOffset;
+        if (payloadSize) {
+          // TODO: process the case partial payload in header packet
+          PacketContainer payloadPackets;
+          auto pit = packets.begin();
+
+          if (pit != packets.end())
+            ++pit; // skip header
+          conn.SetPacketMetaData(*pit, seqNum, ackNum);
+          for (; pit != packets.end(); ++pit) {
+            payloadPackets.push_back(*pit);
+          }
+
+          conn.UpdateRecvSeq(TcpPacketType::ACK, seqNum, payloadSize);
+          conn.submitRxChann(std::move(payloadPackets));
+        } else {
+          conn.UpdateRecvSeq(TcpPacketType::ACK, seqNum);
+        }
+
+        co_return TcpStackState::SUCCESS;
+      } else {
+        co_return TcpStackState::ERR_ARGUMENT;
+      }
+    } else if (tcpFlags == 0x02) {
+      // SYN
+      auto conn = TcpConnectionT(service->GetLocalAddr(), service->GetPort(),
+                                 src_addr, src_port, executor_);
+      // TODO: check if the connection already exists, if it does, reset it
+
+      conn.state_ = TcpConnectionState::SYN_RECEIVED;
+      auto seqNum = ntohl(*reinterpret_cast<const uint32_t *>(data + 4));
+      conn.UpdateRecvSeq(TcpPacketType::SYN, seqNum);
+
+      auto reply = hdr_pool_->allocate();
+      conn.FillPacketIpTcpHdr(TcpPacketType::SYN_ACK, *reply);
+      std::forward_list<asio::const_buffer> r = { reply->GetConstBuf() };
+      auto ret = co_await tx_callback_(r);
+      if (ret >= reply->GetUsedBytes()) {
+        seqNum = this->InitialConnSeq(service->GetLocalAddr(),
+                                      service->GetPort(), src_addr, src_port);
+        conn.UpdateSentSeq(TcpPacketType::SYN_ACK, seqNum, 1);
+        conn.FreshActivity();
+
+        service->AddConnection(std::move(conn));
+        co_return TcpStackState::SUCCESS;
+      }
+      co_return TcpStackState::DROP;
+    } else {
+      co_return TcpStackState::DROP;
+    }
+  }
+};
+
+template <typename AddrType,
+          typename TcpConnectionT = TcpConnectionChan<AddrType>,
+          typename TcpServiceT = TcpService<AddrType, TcpConnectionT>,
+          typename NetworkIOObjectT>
+auto
+MakeAsyncTcpStack(asio::any_io_executor &ex, NetworkIOObjectT &net_io,
+                  shared_netbuf_pool_t hdr_pool)
+{
+  return AsyncUserspaceTcpStack<AddrType, TcpConnectionT, TcpServiceT,
+                                NetworkIOObjectT>(ex, net_io, hdr_pool);
+}
+
+} // namespace netio
+} // namespace celaratcp
+
+#endif
