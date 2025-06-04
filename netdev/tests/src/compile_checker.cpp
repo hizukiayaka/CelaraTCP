8a91c67815e2cf7b844d45ca7873813816f3c111
Randy Li <ayaka@soulik.info>
2025-06-05 00:34:20 +0800

    netdev: tests: invoke the compiling of async tcp
    We are trying include the async tcp header, the compiler could
    tell us some obvious errors.
    
    Until you invoke its procedure, the real error is hiding.

--- netdev/tests/src/compile_checker.cpp
+++ netdev/tests/src/compile_checker.cpp
@@ -3,11 +3,15 @@
  * SPDX-FileCopyrightText: Hsia-Jun(Randy) Li
  */
 
+#include <coroutine>
 #include <net_packet.hpp>
 #include <worker_interface.hpp>
 
 #include "virtual_netdev.hpp"
 #include "userspace_tcp_stack_helper.hpp"
+#include <asio.hpp>
+#include <asio/awaitable.hpp>
+#include <asio/experimental/coro.hpp>
 
 using namespace celaratcp;
 
@@ -24,13 +28,14 @@ public:
   ~manger() = default;
 };
 
-int
-main(int argc, char *argv[])
+int main1(int argc, char *argv[])
 {
+#if 0
   asio::io_context ioc;
 
   asio::ip::network_v4 net1(asio::ip::make_address_v4("169.254.3.1"), 32);
   netdev::VirtualNetDev tun(ioc, "test0", net1.address());
+  netdev::IPacketFilter *filter = tun;
 
   SyncUserspaceTcpStack<asio::ip::address_v4> tcpStack;
   tcpStack.addSimpleService(5162);
@@ -75,6 +80,51 @@ main(int argc, char *argv[])
 
   tun.async_read(packets, testCallback);
   tun.async_read(mbufs, testCallback);
+#endif
+  return 0;
+}
+
+asio::awaitable<void> run2()
+{
+  auto executor = co_await asio::this_coro::executor;
+
+  asio::ip::network_v4 net1(asio::ip::make_address_v4("169.254.3.1"), 32);
+  netdev::VirtualNetDev tun(executor, "test0", net1.address());
+  netdev::IPacketFilter *filter = tun;
+
+  recycle::shared_pool<Ipv4TcpHdrPacket> hdrPool;
+  hdrPool.reserve(20);
+
+  AsyncUserspaceTcpStack<asio::ip::address_v4> tcpStack(executor, std::ref(tun), hdrPool);
+  tcpStack.setLocalAddress(net1.address());
+  tcpStack.addSimpleService(5162);
+
+  recycle::shared_pool<NetPacketSW<regularMtu - ipv4HdrSize>> payloadPool;
+  payloadPool.reserve(20);
+
+  auto hdr = hdrPool.allocate();
+  auto payload = payloadPool.allocate();
+  std::list<asio::mutable_buffer> mbufs = {hdr->getMutableBuf(), payload->getMutableBuf()};
+
+  auto length = co_await tun.async_read(mbufs);
+  if (length > 0) {
+    std::vector<std::shared_ptr<NetPacket>> packets = {hdr, payload};
+    co_await tcpStack.processIncomingPackets(packets);
+  }
+}
+
+int main2(int argc, char *argv[])
+{
+  asio::io_context ioc;
+  asio::co_spawn(ioc, run2(), asio::detached);
+
+  return 0;
+}
+
+int main(int argc, char *argv[])
+{
+  main1(argc, argv);
+  main2(argc, argv);
 
   return 0;
 }
