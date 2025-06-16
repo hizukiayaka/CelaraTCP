405497a3b67bf5388dbabf5b5aef2b61a34b4cbc
Randy Li <ayaka@soulik.info>
2025-06-17 00:30:00 +0800

    netdev: tests: chunk allocator test

--- netdev/tests/src/compile_checker.cpp
+++ netdev/tests/src/compile_checker.cpp
@@ -122,7 +122,7 @@ testSimpleAllocPool()
     memmanger::SimpleHeapAllocator<NetMemChunk> alloc(40);
 
     recycle::shared_pool<NetMemChunk> pool(
-        [&alloc]() { return alloc.allocation(); });
+        [&alloc]() { return alloc.Allocation(); });
     pool.reserve(2);
 
     auto packet = pool.allocate();
@@ -139,7 +139,7 @@ testSimpleAllocPool()
     memmanger::SimpleVectorAllocator<NetMemChunk> alloc(40);
 
     recycle::shared_pool<NetMemChunk> pool(
-        [&alloc]() { return alloc.allocation(); });
+        [&alloc]() { return alloc.Allocation(); });
     pool.reserve(2);
 
     auto packet = pool.allocate();
@@ -150,6 +150,49 @@ testSimpleAllocPool()
       std::cout << "free " << pool.unused_resources() << std::endl;
     }
     pool.free_unused();
+  }
+
+  {
+    std::vector<uint8_t> mems(2000);
+    {
+      auto alloc = memmanger::ManagedMemAllocator<NetMemChunk>::Create(
+          mems.data(), mems.capacity(), 40, [&mems]() {
+            mems.clear();
+            mems.shrink_to_fit();
+          });
+
+      auto pool = std::make_shared<recycle::shared_pool<NetMemChunk> >(
+          [&alloc]() { return alloc->Allocation(); });
+      pool->reserve(20);
+
+      auto packet = pool->allocate();
+      std::cout << "container size is " << mems.size() << std::endl;
+      {
+        std::cout << "free " << pool->unused_resources() << std::endl;
+        auto packet = pool->allocate();
+        std::cout << "free " << pool->unused_resources() << std::endl;
+        packet = pool->allocate();
+        packet = pool->allocate();
+      }
+      std::cout << "packet location is " << packet->GetId() << std::endl;
+      packet = pool->allocate();
+      std::cout << "packet location is " << packet->GetId() << std::endl;
+      pool->free_unused();
+
+      try {
+        pool->reserve(50);
+      }
+      catch (const std::bad_alloc &e) {
+        std::cout << "can't allocate so many buffers: " << e.what()
+                  << std::endl;
+      }
+      catch (...) {
+        std::cout << "an unknown issue" << std::endl;
+      }
+    }
+
+    std::cout << "clear mem, now container size is " << mems.size()
+              << " capacity is " << mems.capacity() << std::endl;
   }
 
   return 0;
